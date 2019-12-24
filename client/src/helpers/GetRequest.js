65fbc61fd0ff9bbade7f41eb27107413445a7a4d
Matt Locklin <mattlocklin@live.co.uk>
2019-12-24 18:19:29 +0000

    prod url

--- client/src/helpers/GetRequest.js
+++ client/src/helpers/GetRequest.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// const url = 'http://localhost:5000/';
+const url = 'https://telford-pool-back-end.herokuapp.com/';
 
 export function useGetRequest(endpoint) {
     const [data, setData] = useState()
 
     useEffect(() => {
         axios
-            .get(`http://localhost:5000/${endpoint}`)
+            .get(`${url}${endpoint}`)
             .then(response => {
                 setData(response)
             })
