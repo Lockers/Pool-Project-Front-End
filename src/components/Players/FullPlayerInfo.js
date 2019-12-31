2730bff8cb7f87fd55292c57a35a79b61dd790ca
Matt Locklin <mattlocklin@live.co.uk>
2019-12-31 16:42:03 +0000

    styled results a bit

--- src/components/Players/FullPlayerInfo.js
+++ src/components/Players/FullPlayerInfo.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Spin } from 'antd';
+import { Spin, Descriptions } from 'antd';
 import Moment from 'react-moment';
+import Styled from 'styled-components';
+
+const Div = Styled.div`
+margin: 1rem;
+`
 
 export const FullPlayerInfo = (props) => {
     
@@ -16,17 +21,18 @@ export const FullPlayerInfo = (props) => {
             <p>Played: {props.player[0].played}</p>
             <p>Won: {props.player[0].won}</p>
             <p>Total Prize Money Won: {props.player[0].totalPrizeMoney}</p>
-            Results
+            <h1>Previous results</h1>
             {props.player[0].results.map((result, index) => {
                 return (
-                    <div key={index}>
-                        <span>{result.challenger}</span>
-                    <span>{result.challengerScore}</span>
-                    <span>{result.challengedScore}</span>
-                    <span>{result.challenged}</span>
-                        <span>{result.venue}</span>
-                        <span><Moment format="DD/MM/YYYY">{result.date}</Moment></span>
-                        </div>
+                    <Div>
+                    <Descriptions key={index} bordered={true} size='small' layout='vertical' column='xs'>
+                        <Descriptions.Item label="Venue">{result.venue}</Descriptions.Item>
+                        <Descriptions.Item label="Date"><Moment format="DD/MM/YYYY">{result.date}</Moment></Descriptions.Item>
+                        <Descriptions.Item label="Ruleset">{result.ruleset}</Descriptions.Item>
+                        <Descriptions.Item label="Pot">£{result.pot}</Descriptions.Item>
+                        <Descriptions.Item label="Result">{result.challenger} {result.challengerScore} - {result.challengedScore} {result.challenged}</Descriptions.Item>
+                    </Descriptions>
+                    </Div>
                 )
             })}
         </div>
