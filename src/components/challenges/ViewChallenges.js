78fdc7b754539035e20843957655e77e8f00c7d4
Matt Locklin <mattlocklin@live.co.uk>
2020-01-12 21:30:29 +0000

    Changed Players Card Color

--- src/components/challenges/ViewChallenges.js
+++ src/components/challenges/ViewChallenges.js
@@ -29,7 +29,7 @@ const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
+        backgroundColor: 'lightgrey',
     },
 }));
 
